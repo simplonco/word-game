78b818330444dfab843c9b8726e3ed1ad18005ae
Sara Guerric <sara.guerric@evergig.com>
2016-10-05 10:16:27 +0200

    Add: word-game

---
+++ index.js
@@ -0,0 +1,21 @@
+// Les deux mots de cette chaine de caractère ont une lettre mal placées
+
+var desordre = 'scaue tomtea'; //'tomate'
+
+// Les strings peuvent être séparer au niveau d'un caractère grace à la méthode .split()
+// exemple:
+// var dream = 'white rabbit';
+// var dreamArray = dream.split(' ');
+// dreamArray prend alors la valeur de : ['white', 'rabbit'];
+// dreamArray[0] a donc la valeur de 'white'
+// dreamArray[1] a la valeur de 'rabbit'
+// Séparer les deux mot de désordre grace à la méthode .split();
+
+/* TON CODE ICI */
+
+// On a du coup à priori deux variables. Une variable sauce et une variable tomate.
+// On veut les remettre dans l'ordre en utilisant la méthode .slice().
+// sauce = 'scaue';
+// var sauce = sauce.slice(0,1) + sauce.slice(2,4) + sauce.slice(1,2) + sauce.slice(4);
+
+/* TON CODE ICI */
